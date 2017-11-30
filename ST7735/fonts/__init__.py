842e7e5b7b114dcd0fcbe7f8611ff549d0af7db7
Fuzzy <fuzzymannerz@users.noreply.github.com>
2017-11-30 10:31:45 +0100

    Update __init__.py

--- ST7735/fonts/__init__.py
+++ ST7735/fonts/__init__.py
@@ -1,5 +1,5 @@
-# Python ST7735 library for Raspberry Pi
-# Copyright 2016-2017 jackw01
+# Python ST7735 library for Orange Pi Zero
+# Copyright 2016-2017 jackw01 / FuzzyTek
 # Distributed under the MIT license
 
 __all__ = ["font4x5", "font5x7", "font7x10"]
