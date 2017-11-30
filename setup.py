0480f53a65ed5e34bf3c37df4e1e918ce4eeb19e
Fuzzy <fuzzymannerz@users.noreply.github.com>
2017-11-30 10:27:29 +0100

    Orange pi zero reqs

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ if sys.version_info < (3, 3, 0):
     raise RuntimeError("This library requires Python 3.3.0+")
 
 setup(
-    name="raspi-python-st7735",
+    name="opiz ST7735",
     version="1.0",
-    description="Python library for using ST7735-based TFT LCDs with a Raspberry Pi",
-    url="https://github.com/jackw01/raspi-python-st7735",
-    author="jackw01",
+    description="Python library for using ST7735-based TFT LCDs with an Orange Pi Zero",
+    url="https://github.com/fuzzymannerz/opizST7735",
+    author="Fuzzy",
     license="MIT",
     install_requires = [
-        'RPi.GPIO>=0.6.2',
+        'OPi.GPIO>=0',
         'spidev>=3.0',
         'Pillow>=4.2.0'
     ],
